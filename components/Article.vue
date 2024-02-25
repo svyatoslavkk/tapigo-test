2e2545940ac76ea6d64eb8cacc0ded138e8ae65e
root <root@DESKTOP-DJTQ9LG.localdomain>
2024-02-25 18:31:30 +0300

    fix: header

--- components/Article.vue
+++ components/Article.vue
@@ -1,15 +1,38 @@
 <template>
   <div>
     <Header />
-    <div v-for="(item, index) in articles" :key="index" class="article">
-      <img :src="item.image" />
-      <div class="article-content">
-        <span>{{ item.description }}</span>
-        <div>
-          <p>{{ item.paragraph_1 }}</p>
-          <p>{{ item.paragraph_2 }}</p>
-          <p v-if="item.paragraph_3">{{ item.paragraph_3 }}</p>
+    <div class="main">
+      <div class="pre-header">
+        <div class="path">
+          <button class="gray-text">Журнал</button>
+          <span class="gray-text">/</span>
+          <button class="gray-text">Статьи</button>
+          <span class="gray-text">/</span>
+          <button class="default-text">Бенуа — идеолог новой художественной эпохи</button>
         </div>
+        <div></div>
+      </div>
+      <div class="content">
+        <img class="art-img" :src="openImage" />
+        <h2>{{ articleTitle }}</h2>
+        <div class="quotes-block">
+          <p class="quotes">{{ articleQuotes }}</p>
+          <span>{{ articleQuotesAuthor }}</span>
+        </div>
+        <div v-for="(item, index) in articles" :key="index" class="article">
+          <div class="img" :style="{ maxWidth: item.imageWidth + 'px' }">
+            <img v-if="item.image" class="art-img" :src="item.image" />
+            <span v-if="item.description" class="description">{{ item.description }}</span>
+          </div>
+          <div class="article-content">
+            <div class="paragraphs">
+              <p>{{ item.paragraph_1 }}</p>
+              <p>{{ item.paragraph_2 }}</p>
+              <p v-if="item.paragraph_3">{{ item.paragraph_3 }}</p>
+            </div>
+          </div>
+        </div>
+        <p class="footer-text">Автор: <button class="link">Журнал Тапиго</button></p>
       </div>
     </div>
   </div>
@@ -17,16 +40,130 @@
 
 <script>
 import { articles } from '~/constants';
+import { openImage } from '~/constants';
+import { articleTitle } from '~/constants';
+import { articleQuotes } from '~/constants';
+import { articleQuotesAuthor } from '~/constants';
 
 export default {
   data() {
     return {
       articles: articles,
+      openImage: openImage,
+      articleTitle: articleTitle,
+      articleQuotes: articleQuotes,
+      articleQuotesAuthor: articleQuotesAuthor,
     };
   },
 };
 </script>
 
 <style>
-
+  h2 {
+    font-size: 34px;
+    font-weight: 700;
+  }
+  button {
+    cursor: pointer;
+  }
+  .quotes-block {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    gap: 0.5rem;
+    width: 80%;
+  }
+  .quotes {
+    font-size: 20px;
+    font-weight: 500;
+    font-style: italic;
+  }
+  .path {
+    display: flex;
+    gap: 0.5rem;
+    font-size: 15px;
+  }
+  .content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 32px;
+    max-width: 950px;
+  }
+  .footer-text {
+    color: #c2c2c7;
+    font-style: italic;
+    font-size: 13px;
+  }
+  .link {
+    font-style: italic;
+    font-size: 13px;
+    color: #008afe;
+    text-decoration: underline;
+    transition: 0.3s ease;
+  }
+  .link:hover {
+    color: #000;
+  }
+  .img {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+  }
+  .art-img {
+    border-radius: 8px;
+    max-width: 950px;
+  }
+  .description {
+    color: #c2c2c7;
+    font-style: italic;
+    font-size: 13px;
+    width: 70%;
+    text-align: center;
+  }
+  .main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 32px;
+    padding: 75px 16px 105px 16px;
+    margin: 0;
+  }
+  .article {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 40px;
+    max-width: 825px;
+  }
+  .paragraphs {
+    max-width: 825px;
+    display: flex;
+    flex-direction: column;
+    gap: 48px;
+  }
+  .pre-header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    max-width: 950px;
+    width: 100%;
+  }
+  .gray-text {
+    font-size: 14px;
+    color: #6a7280;
+  }
+  .gray-text:hover,
+  .gray-text:active,
+  .gray-text:focus {
+    color: #000;
+  }
+  .default-text {
+    font-size: 14px;
+    color: #000;
+  }
 </style>
--- components/Header.vue
+++ components/Header.vue
@@ -1,20 +1,29 @@
 <template>
   <header class="header">
-    <div class="flex items-center gap-12">
-      <SearchIcon />
-      <button class="flex items-center gap-2 py-2 px-4 bg-custom-color-1F3B8B rounded-lg">
+    <div class="flex-content">
+      <button class="icon-button search">
+        <SearchIcon />
+      </button>
+      <button class="menu-button">
         <MenuIcon />
-        <span class="text-white font-medium text-base">Сервисы</span>
+      </button>
+      <button class="desktop-button">
+        <span class="menu-icon">
+          <MenuIcon />
+        </span>
+        <span class="text">Сервисы</span>
       </button>
     </div>
     <div>
       <img :src="logo" class="logo" />
     </div>
-    <div class="flex items-center gap-12">
-      <button class="flex items-center gap-2 py-2 px-4 bg-custom-color-1F3B8B rounded-lg">
-        <span class="text-white font-medium text-base">Разместить</span>
+    <div class="flex-content">
+      <button class="desktop-button">
+        <span class="text">Разместить</span>
       </button>
-      <LogoutIcon />
+      <button class="icon-button">
+        <LogoutIcon />
+      </button>
     </div>
   </header>
 </template>
@@ -34,18 +43,98 @@ export default {
 </script>
 
 <style>
+  .text {
+    font-size: 16px;
+    color: #fff;
+    font-weight: 500;
+  }
   .header {
+    position: fixed;
+    top: 0;
+    left: 0;
     display: flex;
     align-items: center;
     justify-content: space-between;
     width: 100%;
     height: 70px;
-    padding: 0 54px;
+    padding: 0 16px;
     background-color: #fff;
+    transition: 0.3s ease;
   }
   .logo {
     width: 70px;
     height: 70px;
     border-radius: 50%;
+  }
+  .flex-content {
+    display: flex;
+    align-items: center;
+    gap: 24px;
+  }
+  .desktop-button {
+    display: none;
+    align-items: center;
+    gap: 0.5rem;
+    border-radius: 0.5rem;
+    padding: 0.5rem 1rem;
+    background-color: #1F3B8B;
+    cursor: pointer;
+    transition: 0.4s ease;
+  }
+  .desktop-button:hover {
+    background-color: #212A4E;
+  }
+  .icon-button svg {
+    stroke: #1F3B8B;
+  }
+  .icon-button svg:hover {
+    stroke: #212A4E;
+  }
+  .button-icon svg,
+  .menu-icon svg {
+    stroke: #fff;
+  }
+  .menu-button {
+    display: flex;
+  }
+  .menu-button svg {
+    stroke: #1F3B8B;
+  }
+  .menu-button svg:hover {
+    stroke: #212A4E;
+  }
+  .menu-button svg,
+  .menu-icon svg {
+    width: 36px;
+    height: 36px;
+  }
+  .icon-button.search {
+    display: none;
+  }
+  @media (min-width: 600px) {
+    .menu-button {
+      display: none;
+    }
+    .desktop-button {
+      display: flex;
+    }
+    .menu-button svg,
+    .menu-icon svg {
+      width: 20px;
+      height: 20px;
+    }
+    .icon-button.search {
+      display: flex;
+    }
+  }
+  @media (min-width: 1280px) {
+    .header {
+      padding: 0 54px;
+    }
+    .flex-content {
+      display: flex;
+      align-items: center;
+      gap: 48px;
+    }
   }
 </style>
